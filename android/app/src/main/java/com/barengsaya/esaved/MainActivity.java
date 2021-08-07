7882817de968000d7466def09df7f589bab2f892
Fahmi Efendi <kiranadinda330@gmail.com>
2021-08-07 22:29:09 +0700

    revisi

--- android/app/src/main/java/com/barengsaya/esaved/MainActivity.java
+++ android/app/src/main/java/com/barengsaya/esaved/MainActivity.java
@@ -1,4 +1,4 @@
-package com.barengsaya.eESAVEDd;
+package com.barengsaya.eEsavedd;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ESAVED";
+    return "Esaved";
   }
 }
